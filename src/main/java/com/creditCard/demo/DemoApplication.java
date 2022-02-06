ec084d8126c946670427f593a65abf753de17740
manish <kumanish2007@gmail.com>
2022-02-06 23:23:24 +0530

    Initial code commit

---
+++ src/main/java/com/creditCard/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.creditCard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Spring boot application started for Rest API");
+    }
+
+}
